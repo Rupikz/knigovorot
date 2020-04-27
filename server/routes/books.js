bee3dc45b0df49ae07bcddb7da6efad2d9f07c13
rupikz <nikolau22233@gmail.com>
2020-04-27 19:26:10 +0300

    page books

--- server/routes/books.js
+++ server/routes/books.js
@@ -7,7 +7,17 @@ books.get('/', async (req, res) => {
   const selectArtGenresQuery = 'SELECT * FROM public.genres WHERE art = \'yes\'';
   const selectNoArtGenresQuery = 'SELECT * FROM public.genres WHERE art = \'no\'';
   const selectPublisherQuery = 'SELECT * FROM public.publisher';
-  const selectBooksQuery = 'SELECT * FROM public.books LIMIT 20';
+  const selectCountBooks = 'SELECT COUNT(*) FROM public.books';
+  const countPages = Array(Math.floor(selectCountBooks / 160));
+  // Тут остановился, надо сделать страницы
+  let selectBooksQuery;
+  if (req.query.genres) {
+    selectBooksQuery = `SELECT * FROM public.books WHERE genre_id=${req.query.genres} LIMIT 16`;
+  } else if (req.query.publisher) {
+    selectBooksQuery = `SELECT * FROM public.books WHERE publisher_id=${req.query.publisher} LIMIT 16`;
+  } else {
+    selectBooksQuery = 'SELECT * FROM public.books LIMIT 16';
+  }
 
   try {
     const rowsGenresArt = await dbQuery.query(selectArtGenresQuery);
@@ -20,7 +30,7 @@ books.get('/', async (req, res) => {
     const booksBd = rowsBooks.rows;
 
     return res.render('books.hbs', {
-      req, genresArt, genresNoArt, publisher, booksBd,
+      req, genresArt, genresNoArt, publisher, booksBd, countPages,
     });
   } catch (error) {
     console.log('Ошибка в загрузке жанров из бд:', error);
