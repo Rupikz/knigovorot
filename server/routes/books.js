db00340b9d1206830ff0d9bb89a61a85cd0e9a8c
rupikz <nikolau22233@gmail.com>
2020-04-29 22:16:28 +0300

    pagination animate

--- server/routes/books.js
+++ server/routes/books.js
@@ -8,43 +8,80 @@ books.get('/', async (req, res) => {
   const selectNoArtGenresQuery = 'SELECT * FROM public.genres WHERE art = \'no\'';
   const selectPublisherQuery = 'SELECT * FROM public.publisher';
   const selectCountBooks = 'SELECT COUNT(*) FROM public.books';
-  const countPages = req.query.pages || 1;
+  const numberPages = Number(req.query.pages || 1);
 
   const createPagination = (pages, allCountBooks) => {
-    const allPages = allCountBooks / 5;
-    const paginationDefault = {
-      previous: false,
-      next: true,
-      active: 2,
-      pages,
+    const maxPages = Math.ceil(allCountBooks / 5);
+    const paginationMenu = {
+      chevronLeft: {
+        page: pages - 3,
+        active: pages >= 3,
+      },
+      chevronRight: {
+        page: pages + 3,
+        active: maxPages - pages > 3,
+      },
     };
-    let pagesVisible;
-    if (pages < 2) {
-      pagesVisible = [1, 2, 3, 4, 5];
-      paginationDefault.active = pages;
-    } else if (allPages - pages < 2) {
-      pagesVisible = [allPages - 2, allPages - 1, allPages, allPages + 1, allPages + 2];
-      // остановился тут, формирование строк
-      // сори придумать лучше не могу
-      paginationDefault.next = false;
-      paginationDefault.previous = true;
-    } else {
-      pagesVisible = [allPages - 4, allPages - 3, allPages - 2, allPages - 1, allPages];
-      paginationDefault.previous = true;
-      paginationDefault.previous = false;
-    }
 
-    paginationDefault.pages = pagesVisible;
-    return paginationDefault;
+    const pagination = [];
+    for (let i = pages; i < pages + 5; i += 1) {
+      if (maxPages - pages < 3) {
+        if (pages === maxPages - 2) {
+          pagination.push({
+            page: i - 1,
+            active: i - 1 === pages,
+          });
+        } else if (pages === maxPages - 1) {
+          pagination.push({
+            page: i - 2,
+            active: i - 2 === pages,
+          });
+        } else {
+          pagination.push({
+            page: i - 3,
+            active: i - 3 === pages,
+          });
+        }
+      } else if (pages <= 3) {
+        if (pages === 1) {
+          pagination.push({
+            page: i,
+            active: i === pages,
+          });
+        } else if (pages === 2) {
+          pagination.push({
+            page: i - 1,
+            active: i - 1 === pages,
+          });
+        } else {
+          pagination.push({
+            page: i - 2,
+            active: i - 2 === pages,
+          });
+        }
+      } else {
+        pagination.push({
+          page: i - 3,
+          active: i === pages,
+        });
+      }
+    }
+    if (maxPages < 5) {
+      for (let i = 0; i < 5 - maxPages; i += 1) {
+        pagination.pop();
+      }
+    }
+    paginationMenu.pagination = pagination;
+    return paginationMenu;
   };
 
   let selectBooksQuery;
   if (req.query.genres) {
-    selectBooksQuery = `SELECT * FROM public.books WHERE genre_id=${req.query.genres} LIMIT 5 OFFSET ${countPages}`;
+    selectBooksQuery = `SELECT * FROM public.books WHERE genre_id=${req.query.genres} LIMIT 5 OFFSET ${(numberPages - 1) * 5}`;
   } else if (req.query.publisher) {
-    selectBooksQuery = `SELECT * FROM public.books WHERE publisher_id=${req.query.publisher} LIMIT 5 OFFSET ${countPages}`;
+    selectBooksQuery = `SELECT * FROM public.books WHERE publisher_id=${req.query.publisher} LIMIT 5 OFFSET ${(numberPages - 1) * 5}`;
   } else {
-    selectBooksQuery = `SELECT * FROM public.books LIMIT 5 OFFSET ${countPages}`;
+    selectBooksQuery = `SELECT * FROM public.books LIMIT 5 OFFSET ${(numberPages - 1) * 5}`;
   }
 
   try {
@@ -58,10 +95,10 @@ books.get('/', async (req, res) => {
     const booksBd = rowsBooks.rows;
     const rowsCount = await dbQuery.query(selectCountBooks);
     const booksCount = rowsCount.rows[0].count;
-    console.log(createPagination(countPages, booksCount));
-
+    const paginationMenu = createPagination(numberPages, booksCount);
+    console.log(paginationMenu);
     return res.render('books.hbs', {
-      req, genresArt, genresNoArt, publisher, booksBd, countPages,
+      req, genresArt, genresNoArt, publisher, booksBd, numberPages, paginationMenu,
     });
   } catch (error) {
     console.log('Ошибка в загрузке жанров из бд:', error);
