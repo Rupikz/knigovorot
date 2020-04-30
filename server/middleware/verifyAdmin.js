541ec4c98f7b615835449a0290bf0213a42ffb67
rupikz <nikolau22233@gmail.com>
2020-04-30 08:00:57 +0300

    middleware check admins

---
+++ server/middleware/verifyAdmin.js
@@ -0,0 +1,19 @@
+import dbQuery from '../db/dbQuery';
+
+const verifyUserAdminPanel = async (req, res, next) => {
+  const checkAdminQuery = `SELECT admin FROM public.users WHERE login = '${req.user.login}'`;
+  try {
+    const rowsSelect = await dbQuery.query(checkAdminQuery);
+    const isAdmin = rowsSelect.rows[0].admin;
+    if (isAdmin) {
+      next();
+    } else {
+      res.redirect('/404');
+    }
+  } catch (error) {
+    console.log(error);
+    res.redirect('/404');
+  }
+};
+
+export default verifyUserAdminPanel;
