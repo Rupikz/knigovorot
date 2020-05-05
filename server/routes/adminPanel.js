a3e5763fa2749c855cf83e98bc032590bd846559
rupikz <nikolau22233@gmail.com>
2020-05-05 13:38:25 +0300

    update url at img and choice themes

--- server/routes/adminPanel.js
+++ server/routes/adminPanel.js
@@ -2,6 +2,7 @@ import express from 'express';
 import multer from 'multer';
 import fs from 'fs';
 import path from 'path';
+import dbQuery from '../db/dbQuery';
 
 
 const upload = multer({
@@ -22,34 +23,66 @@ const fieldsFiles = [{
 const admin = express.Router('/');
 
 admin.post('/', upload.fields(fieldsFiles), (req, res) => {
-  const pathToFile = path.resolve(__dirname, '../config/configPictures.json');
-  console.log(req.files);
   if (req.files) {
-    const fileContents = JSON.parse(fs.readFileSync(pathToFile, 'utf8'));
+    const pathToFilePictures = path.resolve(__dirname, '../config/configPictures.json');
+    const fileContents = JSON.parse(fs.readFileSync(pathToFilePictures, 'utf8'));
 
     if (Object.prototype.hasOwnProperty.call(req.files, 'picture1')) {
-      fileContents.first = req.files.picture1[0].filename;
+      fileContents.first.image = req.files.picture1[0].filename;
     }
     if (Object.prototype.hasOwnProperty.call(req.files, 'picture2')) {
-      fileContents.second = req.files.picture2[0].filename;
+      fileContents.second.image = req.files.picture2[0].filename;
     }
     if (Object.prototype.hasOwnProperty.call(req.files, 'picture3')) {
-      fileContents.third = req.files.picture3[0].filename;
+      fileContents.third.image = req.files.picture3[0].filename;
+    }
+
+    if (req.body.src1 && req.body.src2 && req.body.src3) {
+      fileContents.first.src = req.body.src1;
+      fileContents.second.src = req.body.src2;
+      fileContents.third.src = req.body.src3;
     }
 
     const fileContentsString = JSON.stringify(fileContents);
-    fs.writeFile(pathToFile, fileContentsString, (err) => {
+    fs.writeFile(pathToFilePictures, fileContentsString, (err) => {
       if (err) console.log('error', err);
     });
   }
+
+  if (req.body.genre1 || req.body.genre2) {
+    const pathToFileThemes = path.resolve(__dirname, '../config/configThemes.json');
+    const fileContents = JSON.parse(fs.readFileSync(pathToFileThemes, 'utf8'));
+    if (req.body.genre1) {
+      fileContents.first = req.body.genre1;
+    }
+    if (req.body.genre2) {
+      fileContents.second = req.body.genre2;
+    }
+    const fileContentsString = JSON.stringify(fileContents);
+    fs.writeFile(pathToFileThemes, fileContentsString, (err) => {
+      if (err) console.log('error', err);
+    });
+  }
+
   res.redirect('/admin');
 });
 
-admin.get('/', (req, res) => {
-  const pathToFile = path.resolve(__dirname, '../config/configPictures.json');
-  const fileContents = JSON.parse(fs.readFileSync(pathToFile, 'utf8'));
+admin.get('/', async (req, res) => {
+  const selectGenresQuery = 'SELECT * FROM public.genres';
+  try {
+    const rowsGenres = await dbQuery.query(selectGenresQuery);
+    const genres = rowsGenres.rows;
 
-  res.render('adminPanel.hbs', { req, isAdmin: true, fileContents });
+    const pathToFile = path.resolve(__dirname, '../config/configPictures.json');
+    const fileContents = JSON.parse(fs.readFileSync(pathToFile, 'utf8'));
+
+    return res.render('adminPanel.hbs', {
+      req, isAdmin: true, fileContents, genres,
+    });
+  } catch (error) {
+    console.log('Ошибка в загрузке жанров из бд:', error);
+    return res.render('notfound.hbs');
+  }
 });
 
 export default admin;
--- server/routes/index.js
+++ server/routes/index.js
@@ -9,24 +9,27 @@ import { selectBooksByGenreString } from '../db/dbQueryString';
 const index = express.Router('/');
 
 index.get('/', async (req, res) => {
-  const selectBooksQueryArr = selectBooksByGenreString([5, 2]);
-  const sectionBooks = [];
-  const rowsBooks1 = await dbQuery.query(selectBooksQueryArr[0]);
-  sectionBooks.push({
-    name: 'Искусство, Искусствоведение, Дизайн',
-    books: rowsBooks1.rows,
-  });
-  const rowsBooks2 = await dbQuery.query(selectBooksQueryArr[1]);
-  sectionBooks.push({
-    name: 'Детективы и Триллеры',
-    books: rowsBooks2.rows,
-  });
   try {
-    const pathToFile = path.resolve(__dirname, '../config/configPictures.json');
-    const fileContents = JSON.parse(fs.readFileSync(pathToFile, 'utf8'));
-
+    const pathToFileThemes = path.resolve(__dirname, '../config/configThemes.json');
+    const fileThemes = JSON.parse(fs.readFileSync(pathToFileThemes, 'utf8'));
+    const selectBooksQueryArr = selectBooksByGenreString([fileThemes.first, fileThemes.second]);
+    // работает, сделать вывод названий
+    console.log(selectBooksQueryArr);
+    const sectionBooks = [];
+    const rowsBooks1 = await dbQuery.query(selectBooksQueryArr[0]);
+    sectionBooks.push({
+      name: 'Искусство, Искусствоведение, Дизайн',
+      books: rowsBooks1.rows,
+    });
+    const rowsBooks2 = await dbQuery.query(selectBooksQueryArr[1]);
+    sectionBooks.push({
+      name: 'Детективы и Триллеры',
+      books: rowsBooks2.rows,
+    });
+    const pathToFilePictures = path.resolve(__dirname, '../config/configPictures.json');
+    const filePictures = JSON.parse(fs.readFileSync(pathToFilePictures, 'utf8'));
     return res.render('index.hbs', {
-      req, sectionBooks, fileContents,
+      req, sectionBooks, filePictures,
     });
   } catch (error) {
     console.log('Ошибка в загрузке книг из бд:', error);
